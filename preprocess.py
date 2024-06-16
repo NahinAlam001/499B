589c46f536fe9e757202f281cdeea4d5fa553cc9
Md.Nahin Alam <82433419+NahinAlam001@users.noreply.github.com>
2024-06-16 21:58:08 +0600

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -2,9 +2,6 @@ import os
 import zipfile
 import subprocess
 
-def clone_repo():
-    subprocess.run(["git", "clone", "-b", "SAM", "https://github.com/NahinAlam001/499B.git"])
-
 def download_dataset():
     subprocess.run(["gdown", "https://drive.google.com/uc?id=1FomR78IlcqHqNi77fXAJCMgvEebihhF-"])
 
@@ -13,12 +10,7 @@ def extract_dataset(zip_file, extract_to="."):
         zip_ref.extractall(extract_to)
 
 if __name__ == "__main__":
-    # Clone the repository
-    clone_repo()
-    
-    # Change directory to the cloned repository
-    os.chdir('499B')
-
+  
     # Download the dataset
     download_dataset()
 
